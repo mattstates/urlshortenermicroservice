45c980f3930a56c57ebc814592ce73d432008667
Matt States <mattstates@icloud.com>
2017-05-13 16:37:50 -0700

    fix env.js for prod

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-require('./env.js');
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.port || 3000;
+if (!process.env.port) require('./env.js');
+
 
 ///// Database and Schema /////
 const mongoose = require('mongoose');
